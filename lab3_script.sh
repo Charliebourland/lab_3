b6a213e14c353d96037aa8b81bc8e46faff69cc0
charliebourland <chbo2810@colorado.edu>
2019-09-20 13:30:56 -0600

    Change in practice.sh

---
+++ lab3_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Authors : Charlie Bourland
+# Date: 9/20/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+echo "Enter a File name: "
+read name
+echo "Enter a regular expression: "
+read ex
+grep $ex $name >> email_results.txt
+grep -c $ex $name 
+
+
